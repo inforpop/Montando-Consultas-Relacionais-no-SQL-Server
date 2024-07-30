1194fa2d39679a6253491a6d8db4f855bdc25297
inforpop <42259413+inforpop@users.noreply.github.com>
2024-07-29 23:44:59 -0300

    Update scripts.sql

--- scripts.sql
+++ scripts.sql
@@ -5,3 +5,29 @@ CREATE TABLE Hospedes (
     Telefone NVARCHAR(20),
     Email NVARCHAR(100)
 );
+SELECT 
+    R.ReservaId,
+    H.Nome AS NomeHospede,
+    Q.Numero AS NumeroQuarto,
+    R.DataEntrada,
+    R.DataSaida,
+    R.ValorTotal
+FROM 
+    Reservas R
+INNER JOIN 
+    Hospedes H ON R.HospedeId = H.HospedeId
+INNER JOIN 
+    Quartos Q ON R.QuartoId = Q.QuartoId;
+SELECT 
+    R.ReservaId,
+    H.Nome AS NomeHospede,
+    Q.Numero AS NumeroQuarto,
+    R.DataEntrada,
+    R.DataSaida,
+    R.ValorTotal
+FROM 
+    Reservas R
+LEFT JOIN 
+    Hospedes H ON R.HospedeId = H.HospedeId
+LEFT JOIN 
+    Quartos Q ON R.QuartoId = Q.QuartoId;
